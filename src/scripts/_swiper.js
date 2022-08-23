a1f4095cc264fd7e4efa83ac136b31884d3bdac4
Danil <danil.polovinkin.1994@gmail.com>
2022-08-23 15:57:21 +0300

    added and adaptated header,added slider blank

---
+++ src/scripts/_swiper.js
@@ -0,0 +1,21 @@
+import Swiper, { Navigation, Pagination } from "swiper";
+// import Swiper and modules styles
+
+function slider() {
+  // init Swiper:
+  const swiper = new Swiper(".swiper", {
+    spaceBetween: 0,
+    slidesPerView: 1.2,
+    centeredSlides: true,
+    centeredSlides: false,
+    roundLengths: true,
+    loop: false,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
+}
+
+export default slider;
