c43f8e9b9dded9279845fe5003ba0b559f62b845
Danil <danil.polovinkin.1994@gmail.com>
2022-09-01 14:10:06 +0300

    completed adaptation 375px

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,7 +7,26 @@ window.addEventListener("DOMContentLoaded", () => {
     slider();
 
     window.addEventListener("resize", () => {
-      if (window.innerWidth <= 768) {
+      if (window.innerWidth <= 375) {
+        let galleryWrapper2 = document.querySelector(
+            ".gallery__imgs_texts_2 .gallery__imgs_bottom-text"
+          ),
+          galleryWrapper3 = document.querySelector(
+            ".gallery__imgs_texts_3 .gallery__imgs_bottom-text"
+          ),
+          galleryWrapper4 = document.querySelector(
+            ".gallery__imgs_texts_4 .gallery__imgs_bottom-text"
+          );
+        // galleryWrapper5 = document.querySelector(
+        //   ".gallery__imgs_texts_5 .gallery__imgs_bottom-text"
+        // )
+
+        console.log("375px", window.innerWidth);
+
+        galleryWrapper2.textContent = "Eyewear store interior";
+        galleryWrapper3.textContent = "Minimalist style home";
+        galleryWrapper4.textContent = "Eyewear store interior";
+      } else if (380 < window.innerWidth <= 768) {
         let galleryWrapper1 = document.querySelector(
             ".gallery__imgs_texts_1 .gallery__imgs_bottom-text"
           ),
@@ -22,7 +41,6 @@ window.addEventListener("DOMContentLoaded", () => {
           galleryImg4 = document.querySelector(".img_4"),
           galleryImg5 = document.querySelector(".source_img_5");
 
-        console.log(galleryWrapper3);
         galleryWrapper1.textContent = "Residence by the lighthouse";
         galleryWrapper3.textContent = "Eyewear storeinterior";
         galleryWrapper4.textContent = "House on a Hillside";
@@ -31,6 +49,8 @@ window.addEventListener("DOMContentLoaded", () => {
         galleryImg3.setAttribute("src", "../img/mainPage/Pic_3_change.webp");
         galleryImg4.setAttribute("src", "../img/mainPage/Pic_4_change.webp");
         galleryImg5.setAttribute("srcset", "../img/mainPage/Pic_5_change.webp");
+
+        console.log("768px", window.innerWidth);
       } else {
         let galleryWrapper3 = document.querySelector(
             ".gallery__imgs_wrapper.pic_3"
